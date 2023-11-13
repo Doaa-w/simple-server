42308488f0583145e75a6e6c6ecf36e872904881
.ドウアア. <dia13@outlook.sa>
2023-11-13 11:32:37 +0300

    adjustment

--- express.js
+++ express.js
@@ -25,10 +25,40 @@ app.get('/', (req , res)=>{
 
 })
 
+app.get('/products/:id' ,(req,res)=>{
+    const id = req.params.id;
+    const product= products.find((product)=> product.id === id);
+    if(!product){
+        const error = new Error (`product with ${id} not found`)
+        error.status =404
+        throw error
+    }
+        res.status(200).send({
+            sucsess: true,
+            message:' product is here',
+            payload: product,
+        })  
+})
 
 app.post('/', (req,res) =>{
     try {
         res.status(200).send('Creat a product') 
+    } catch (error) {
+        res.status(500).send('server error')
+    }
+})
+
+app.delete('/products/:id' , (req , res )=>{
+    try {
+        const id = req.params.id;
+        const product= products.find((product)=> product.id === id);
+        if(!product){
+            const error = new Error (`product with ${id} not found`)
+            error.status =404
+            throw error
+        }
+        const filteredProducts=products.filter((product)=> product.id !==id)
+        products = filteredProducts
     } catch (error) {
         res.status(500).send('server error')
     }
@@ -53,7 +83,7 @@ app.post('/products', async(req,res) =>{
     }
     const Products = JSON.parse(await fs.readFile('product.json' , 'utf-8'));
     try {
-        const {title , price} = req.body;
+    const {title , price} = req.body;
      const newProduct= {
         id: new Date().getTime().toString,
         title: title ,
@@ -74,6 +104,29 @@ app.post('/products', async(req,res) =>{
     return;
 })
 
+app.put('/products/:id', async(req,res) =>{
+try {
+    const id = req.params.id;
+    const {title , price} = req.body;
+    const indexProduct = products.find((product)=> product.id === id);
+    if(index == -1){
+        const error = new Error (`product with ${id} is not found`)
+        error.status =404
+        throw error
+    }
+    products[indexProduct].title=title
+    products[indexProduct].price=price
+    res.status(201).json({
+        sucsess: true,
+        message: 'product is updated', 
+    })  
+} catch (error) {
+    res.status(500).send({
+        sucsess: false,
+        message: "server error"
+    })
+}
+})
 
 app.listen(PORT,()=>{
     console.log(`Server running at http://127.0.0.1:${PORT}/`);
